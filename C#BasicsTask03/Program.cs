4410419d330d6db10a6cdf6034bdd4c1c04cc96d
mohamedahmeddevx-web <mohamedahmed.devx@gmail.com>
2026-09-08 11:48:26 +0300

    Question08

--- C#BasicsTask03/Program.cs
+++ C#BasicsTask03/Program.cs
@@ -110,6 +110,25 @@
             }
             #endregion
 
+            #region Question08
+            //Using the same books array, print every title except
+            //"The Pragmatic Programmer"(skip it with continue, don't stop the loop).
+
+            Console.WriteLine("");
+            Console.WriteLine("Question08 : ");
+            string[] bookList4 = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+            foreach (string book in bookList4)
+            {
+                if (book == "The Pragmatic Programmer")
+                {
+                    continue;
+                }
+                Console.WriteLine(book);
+            }
+
+
+            #endregion
+
             Console.ReadLine();
         }
     }
