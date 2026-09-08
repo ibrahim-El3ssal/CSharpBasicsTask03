74180ce9b5df05f8abb872c1aa28dfe919f12b3c
mohamedahmeddevx-web <mohamedahmed.devx@gmail.com>
2026-09-08 11:45:32 +0300

    Question03

--- C#BasicsTask03/Program.cs
+++ C#BasicsTask03/Program.cs
@@ -37,6 +37,20 @@
 
             #endregion
 
+            #region Question03
+            //Given string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };,
+            //use a for loop to print each book with its position number, like 1.Clean Code.\
+
+            Console.WriteLine("");
+            Console.WriteLine("Question03 : ");
+
+            string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+            for (int i = 0; i < books.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}.{books[i]}");
+            }
+            #endregion
+
 
             Console.ReadLine();
         }
